656889b4f6d1042861ee7caab413160970164462
Sindhuja Pandian <51230035+SindhujaPandian@users.noreply.github.com>
2021-12-16 10:40:32 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -12,21 +12,13 @@ def index(request):
 def result(request):
     username = request.POST['username']
     keyword = request.POST['keyword']
-    print('-----------------------------')
-    print(username)
-    print(keyword)
-    print('-----------------------------')
     result = mlmodel(keyword)
-    print(result)
     if(result=='NEUTRAL'):
-        #res_dict={'result_NEUTRAL':'https://img.icons8.com/emoji/452/neutral-face.png','NEU':'NEUTRAL'}
         res_dict={'result':'NEUTRAL'}
     elif(result=='POSITIVE'):
-        #res_dict={'result_POSITIVE':'https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_960_720.png','POS':'POSITIVE'}
         res_dict={'result':'POSITIVE'}
     elif(result=='NEGATIVE'):
         res_dict={'result':'NEGATIVE'}
-
     return render(request,'app/result.html',res_dict)
 
 def accuracy(request):
